60b7e32409547be0da3371d38e9c53989ccc9d82
Karuppiah Natarajan <karuppiah.natarajan@capillarytech.com>
2025-09-01 18:33:59 +0530

    fix the add monitors script

--- js/uptime-kuma-api-client/v2/add-all-monitors.js
+++ js/uptime-kuma-api-client/v2/add-all-monitors.js
@@ -1,6 +1,6 @@
 const utils = require('../utils.js')
 const { io } = require("socket.io-client");
-const fs = require('fs')
+const fs = require('fs');
 
 console.log("starting now")
 
@@ -15,67 +15,77 @@ socket.on("monitorList", (data) => {
     fs.writeFileSync(`list-of-monitors-${Date.now()}.json`, JSON.stringify(data, null, 2))
 })
 
+// mapping of Old Monitor ID to New Monitor ID
+const monitorsAdded = {}
+
+const oldMonitors = JSON.parse(String(fs.readFileSync("all-monitors.json")))
+
+function addMonitorsWithParent(oldMonitorIDOfTheParentToLookFor) {
+    for (const oldMonitorID in oldMonitors) {
+        console.log(`current monitors added: ${JSON.stringify(monitorsAdded)}`);
+
+        if (oldMonitorID in monitorsAdded) {
+            continue
+        }
+
+        const oldMonitor = oldMonitors[oldMonitorID]
+
+        // Only add group monitors for now
+        if (oldMonitor.type !== "group") {
+            monitorsAdded[oldMonitorID] = "some-dummy-bad-non-number-value"
+            continue
+        }
+
+        if (oldMonitor.parent != oldMonitorIDOfTheParentToLookFor) {
+            continue
+        }
+
+        let parent = null
+
+        if (oldMonitor.parent) {
+            if (oldMonitor.parent in monitorsAdded) {
+                parent = monitorsAdded[oldMonitor.parent]
+            } else {
+                console.log("Found a parent that is NOT added yet!");
+                continue
+            }
+        }
+
+        const addMonitorRequestData = {
+            type: oldMonitor.type,
+            name: oldMonitor.name,
+            accepted_statuscodes: [], // Not used I think. Check once and verify
+            notificationIDList: [], // Not used I think. Check once and verify
+            interval: oldMonitor.interval,
+            maxretries: oldMonitor.maxretries,
+            retryInterval: oldMonitor.retryInterval,
+            resendInterval: oldMonitor.resendInterval,
+            upsideDown: oldMonitor.upsideDown,
+            parent: parent,
+            description: oldMonitor.description,
+        }
+
+        console.log(addMonitorRequestData);
+
+        socket.emit("add", addMonitorRequestData, (response) => {
+            console.log(`Monitor Addition Response: ${JSON.stringify(response)}`)
+
+            if (response.ok) {
+                console.log("Monitor Added Successfully")
+                const newMonitorID = response.monitorID
+                monitorsAdded[oldMonitorID] = newMonitorID
+                addMonitorsWithParent(oldMonitorID)
+
+                console.log(`updated monitors added: ${JSON.stringify(monitorsAdded)}`);
+            }
+        })
+    }
+}
+
 socket.on("loginRequired", () => {
     console.log(`Server says login is required`)
     utils.login(socket, () => {
-        const monitorsAdded = {}
-
-        const monitors = JSON.parse(String(fs.readFileSync("all-monitors.json")))
-
-        while(monitors.length !== Object.keys(monitorsAdded).length) {
-
-            for (const monitorID in monitors) {
-                if (monitorID in monitorsAdded) {
-                    continue
-                }
-
-                const monitor = monitors[monitorID]
-
-                // Only add group monitors for now
-                if (monitor.type !== "group") {
-                    monitorsAdded[monitorID] = "some-dummy-bad-non-number-value"
-                    continue
-                }
-
-                let parent = null
-
-                if (monitor.parent) {
-                    if (monitor.parent in monitorsAdded) {
-                        parent = monitorsAdded[monitor.parent]
-                    } else {
-                        console.log("Found a parent that is NOT added yet!");
-                        continue
-                    }
-                }
-
-                const addMonitorRequestData = {
-                    type: monitor.type,
-                    name: monitor.name,
-                    accepted_statuscodes: [], // Not used I think. Check once and verify
-                    notificationIDList: [], // Not used I think. Check once and verify
-                    interval: monitor.interval,
-                    maxretries: monitor.maxretries,
-                    retryInterval: monitor.retryInterval,
-                    resendInterval: monitor.resendInterval,
-                    upsideDown: monitor.upsideDown,
-                    parent: parent,
-                    description: monitor.description,
-                }
-
-                console.log(addMonitorRequestData);
-
-                socket.emit("add", addMonitorRequestData, (response) => {
-                    console.log(`Monitor Addition Response: ${JSON.stringify(response)}`)
-
-                    if (response.ok) {
-                        console.log("Monitor Added Successfully")
-                        monitorsAdded[monitorID] = response.monitorID
-
-                        console.log(`monitors added: ${JSON.stringify(monitorsAdded)}`);
-                    }
-                })
-            }
-        }
+        addMonitorsWithParent(null)
     })
 })
 
