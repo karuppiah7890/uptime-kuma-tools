12579da59032ba0c83e7f2cfef9b8e1887fe2396
Karuppiah Natarajan <karuppiah7890@gmail.com>
2025-09-10 11:03:00 +0530

    add example script to add status pages

---
+++ js/uptime-kuma-api-client/v2/add-status-page-example.js
@@ -0,0 +1,157 @@
+const utils = require('../utils.js')
+const { io } = require("socket.io-client")
+const fs = require('fs')
+
+console.log("starting now")
+
+const socket = io(process.env.UPTIME_KUMA_URI)
+
+console.log("listening for events")
+
+utils.listenOnSocketManager(socket)
+
+const statusPagesToAdd = [
+  {
+    slug: "incrm",
+    title: "INCRM Status Page",
+    description: "",
+    logo: "/icon.svg",
+    theme: "auto",
+    showTags: false,
+    domainNameList: [],
+    customCSS: "body {\n\n\n}\n",
+    footerText: "Capillary Technologies",
+    showPoweredBy: false,
+    googleAnalyticsId: null,
+    showCertificateExpiry: false,
+    autoRefreshInterval: 60
+  },
+  {
+    slug: "tatacrm",
+    title: "TATACRM Status Page",
+    description: "",
+    logo: "/icon.svg",
+    theme: "auto",
+    showTags: false,
+    domainNameList: [],
+    customCSS: "body {\n\n\n}\n",
+    footerText: "Capillary Technologies",
+    showPoweredBy: false,
+    googleAnalyticsId: null,
+    showCertificateExpiry: false,
+    autoRefreshInterval: 60
+  },
+  {
+    slug: "eucrm",
+    title: "EUCRM Status Page",
+    description: "",
+    logo: "/icon.svg",
+    theme: "auto",
+    showTags: false,
+    domainNameList: [],
+    customCSS: "body {\n\n\n}\n",
+    footerText: "Capillary Technologies",
+    showPoweredBy: false,
+    googleAnalyticsId: null,
+    showCertificateExpiry: false,
+    autoRefreshInterval: 60
+  },
+  {
+    slug: "uscrm",
+    title: "USCRM Status Page",
+    description: "",
+    logo: "/icon.svg",
+    theme: "auto",
+    showTags: false,
+    domainNameList: [],
+    customCSS: "body {\n\n\n}\n",
+    footerText: "Capillary Technologies",
+    showPoweredBy: false,
+    googleAnalyticsId: null,
+    showCertificateExpiry: false,
+    autoRefreshInterval: 60
+  },
+  {
+    slug: "asiacrm",
+    title: "ASIACRM Status Page",
+    description: "",
+    logo: "/icon.svg",
+    theme: "auto",
+    showTags: false,
+    domainNameList: [],
+    customCSS: "body {\n\n\n}\n",
+    footerText: "Capillary Technologies",
+    showPoweredBy: false,
+    googleAnalyticsId: null,
+    showCertificateExpiry: false,
+    autoRefreshInterval: 60
+  },
+  {
+    slug: "nightly",
+    title: "Nightly Status Page",
+    description: "",
+    logo: "/icon.svg",
+    theme: "auto",
+    showTags: false,
+    domainNameList: [],
+    customCSS: "body {\n\n\n}\n",
+    footerText: "Capillary Technologies",
+    showPoweredBy: false,
+    googleAnalyticsId: null,
+    showCertificateExpiry: false,
+    autoRefreshInterval: 60
+  },
+  {
+    slug: "staging",
+    title: "Staging Status Page",
+    description: "",
+    logo: "/icon.svg",
+    theme: "auto",
+    showTags: false,
+    domainNameList: [],
+    customCSS: "body {\n\n\n}\n",
+    footerText: "Capillary Technologies",
+    showPoweredBy: false,
+    googleAnalyticsId: null,
+    showCertificateExpiry: false,
+    autoRefreshInterval: 60
+  }
+]
+
+function addStatusPage(title, slug) {
+    return new Promise((resolve, reject) => {
+        console.log(`adding status page slug ${slug}`)
+        socket.emit("addStatusPage", title, slug, (response) => {
+            console.log(`response for adding status page slug ${slug}: ${JSON.stringify(response)}`)
+
+            if (response.ok) {
+                console.log(`added status page ${slug} successfully: ${JSON.stringify(response)}`)
+                resolve()
+            } else {
+                reject(new Error(`error adding status page ${slug}! response: ${JSON.stringify(response, null, 2)}`))
+            }
+        })
+    })
+}
+
+socket.on("statusPageList", async (statusPages) => {
+    console.log(`Status Pages List is: ${JSON.stringify(statusPages)}`)
+    fs.writeFileSync(`list-of-status-pages-${Date.now()}.json`, JSON.stringify(statusPages, null, 2))
+
+    for (const statusPage of statusPagesToAdd) {
+        console.log(`processing status page: ${JSON.stringify(statusPage)}`);
+
+        try {
+            await addStatusPage(statusPage.title, statusPage.slug)
+        } catch(err) {
+            throw err
+        }
+    }
+})
+
+socket.on("loginRequired", () => {
+    console.log(`Server says login is required`)
+    utils.login(socket)
+})
+
+utils.listenOnSocket(socket)
