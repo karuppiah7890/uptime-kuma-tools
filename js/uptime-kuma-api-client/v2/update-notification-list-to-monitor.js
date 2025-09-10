fa1bfac6baac7b8fef41c58a5d787fe94a46bf5d
Karuppiah Natarajan <karuppiah7890@gmail.com>
2025-09-10 21:49:30 +0530

    add pseudo code for updating notification list to monitor

---
+++ js/uptime-kuma-api-client/v2/update-notification-list-to-monitor.js
@@ -0,0 +1,13 @@
+// get old monitors list (in v1)
+
+// get new monitors list (in v2)
+
+// iterate through each old monitor {
+//     if old monitor has notifications, get the notification IDs (in v1) and check if it's enabled
+//     get the new notification ID (in v2) given the old notification ID (in v1)
+
+//     get the corresponding new monitor given the old monitor ID and the mapping of old monitor ID to new monitor ID
+
+//     modify local new monitor with notifications - using the new notification ID
+//     save the new monitor to uptime kuma
+// }
